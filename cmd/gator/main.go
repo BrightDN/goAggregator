b1e942d79a11ae86ff2219b85970ec1bc68f0a26
brightDN <brightondeneef@gmail.com>
2025-09-21 14:34:31 +0200

    updating file system

---
+++ cmd/gator/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	_ "github.com/lib/pq"
+	"github.com/BrightDN/goAggregator/internal/config"
+	"github.com/BrightDN/goAggregator/internal/commands"
+    "github.com/BrightDN/goAggregator/internal/database"
+	"fmt"
+	"os"
+    "database/sql"
+)
+
+
+func main() {
+    cfg, err := config.Read()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "read config: %v\n", err)
+        os.Exit(1)
+    }
+
+    db, err := sql.Open("postgres", cfg.DBURL)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Database connection: %w\n", err)
+    }
+
+    defer db.Close()
+
+    dbQueries := database.New(db)
+
+    state := config.State{
+        Db: dbQueries,
+        Cfg: &cfg,
+    }
+    cmds := commands.Commands{
+        AvailableCommands: make(map[string]func(*config.State, commands.Command) error),
+    }
+    cmds.Register("login", commands.HandlerLogin)
+    cmds.Register("register", commands.HandlerRegister)
+    cmds.Register("reset", commands.HandlerReset)
+    cmds.Register("users", commands.HandlerGetUsers)
+    cmds.Register("agg", commands.HandlerFetchFeed)
+    cmds.Register("addfeed", middlewareLoggedIn(commands.HandlerAddFeed))
+    cmds.Register("feeds", commands.HandlerGetFeeds)
+    cmds.Register("following", middlewareLoggedIn(commands.HandlerGetAllFollowings))
+    cmds.Register("follow", middlewareLoggedIn(commands.HandlerFollow))
+    cmds.Register("unfollow", middlewareLoggedIn(commands.Handlerunfollow))
+    cmds.Register("browse", middlewareLoggedIn(commands.HandlerBrowse))
+
+    if len(os.Args) < 2 {
+        fmt.Fprintln(os.Stderr, "not enough arguments")
+        os.Exit(1)
+    }
+
+    cmd := commands.Command{
+        Name: os.Args[1],
+        Args: os.Args[2:],
+    }
+
+    if err := cmds.Run(&state, cmd); err != nil {
+        fmt.Fprintln(os.Stderr, err)
+        os.Exit(1)
+    }
+}
